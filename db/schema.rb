2e63f002f20fdc415ee0aaec1fb298ba3e2a9c31
Sean Doyle <sean.p.doyle24@gmail.com>
2021-04-06 09:29:29 -0400

    [SKIP]: Scaffold Messages
    ```sh
    bin/rails generate scaffold Message
    bin/rails db:migrate
    ```

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_30_002723) do
+ActiveRecord::Schema.define(version: 2021_08_30_003004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -52,6 +52,11 @@ ActiveRecord::Schema.define(version: 2021_08_30_002723) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
