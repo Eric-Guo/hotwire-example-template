93fdc16a07a896acda4f21f1c11dacd0a00ee544
Sean Doyle <sean.p.doyle24@gmail.com>
2021-09-13 11:40:34 -0400

    Add support for hiding fields

--- app/javascript/controllers/fields_controller.js
+++ app/javascript/controllers/fields_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 import { TemplateInstance } from "https://cdn.skypack.dev/@github/template-parts"
 
 export default class extends Controller {
-  static get targets() { return [ "template" ] }
+  static get targets() { return [ "fieldset", "template" ] }
   static get values() { return { placeholder: String } }
 
   insert({ currentTarget }) {
@@ -10,5 +10,14 @@ export default class extends Controller {
     const template = new TemplateInstance(this.templateTarget, { [this.placeholderValue]: id })
 
     currentTarget.before(template)
+  }
+
+  hide({ target }) {
+    const fieldset = this.fieldsetTargets.find(fieldset => fieldset.contains(target))
+
+    if (fieldset) {
+      fieldset.hidden = true
+      fieldset.disabled = true
+    }
   }
 }
