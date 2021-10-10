85ad0e28c7eb8bd949b14aacae59a2a14fcadab2
Sean Doyle <sean.p.doyle24@gmail.com>
2021-10-10 16:20:00 -0400

    Focus preservation

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,15 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "trix"
 import "@rails/actiontext"
+
+addEventListener("turbo:submit-start", (submitStart) => {
+  const { formElement, submitter } = submitStart.detail.formSubmission
+
+  if (submitter) submitter.disabled = true
+
+  formElement.addEventListener("turbo:submit-end", (submitEnd) => {
+    const { formElement, submitter } = submitEnd.detail.formSubmission
+
+    if (submitter) submitter.disabled = false
+  }, { once: true })
+})
